7c5dc3a718db8a4e906401912b66db63ea503c06
FeerTT <fernandotrillobaron25@hotmail.com>
2024-01-31 17:54:37 -0300

    Remove @NotEmpty on create-team.dto

--- src/modules/team/controllers/dto/create-team.dto.ts
+++ src/modules/team/controllers/dto/create-team.dto.ts
@@ -1,46 +1,36 @@
 import { IsString, IsNotEmpty, IsNumber, IsEmail } from 'class-validator';
 
 export default class CreateTeamDto {
-  @IsNotEmpty()
   @IsString()
   name: string;
 
-  @IsNotEmpty()
   @IsString()
   shortName: string;
 
-  @IsNotEmpty()
   @IsString()
   tla: string;
 
-  @IsNotEmpty()
   @IsString()
   address: string;
 
-  @IsNotEmpty()
   @IsString()
   phone: string;
 
-  @IsNotEmpty()
   @IsString()
   website: string;
 
-  @IsNotEmpty()
   @IsEmail()
   email: string;
 
-  @IsNotEmpty()
   @IsNumber()
   founded: number;
 
-  @IsNotEmpty()
   @IsString()
   clubColors: string;
 
   @IsString()
   venue: string;
 
-  @IsNotEmpty()
   @IsString()
   crestUrl: string;
 }
